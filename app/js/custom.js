c006a1dd1a78f289b6895082c705e60c4207d3ed
Yury Kirilyuk <yury@cyberiansoft.com>
2018-08-07 19:55:12 +0300

    form

--- app/js/custom.js
+++ app/js/custom.js
@@ -9,7 +9,8 @@ $(function(){
     $('.main-slider').slick({
         dots: true,
         //infinite: false,
-        speed: 300,
+        speed: 400,
+        //fade: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         adaptiveHeight: false,
@@ -23,6 +24,32 @@ $(function(){
                     slidesToScroll: 1,
                     infinite: true,
                     dots: true
+                }
+            }/*,
+            {
+                breakpoint: 600,
+                settings: "unslick"
+            }*/
+        ]
+    });
+
+    $('.community-slider').slick({
+        dots: false,
+        speed: 700,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        adaptiveHeight: false,
+        arrows: true,
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        responsive: [
+            {
+                breakpoint: 425,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true
                 }
             }/*,
             {
