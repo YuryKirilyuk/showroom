e1121e04a55cb6987b4e3ff4e74fb0b1b717690b
Yury Kirilyuk <yury@cyberiansoft.com>
2018-08-10 17:53:16 +0300

    sign in, event

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,10 +1,5 @@
 $(function(){
 
-	$('.button-nav').click(function(){
-		$(this).toggleClass('active'),
-		$('.main-nav-list').slideToggle();
-		return false;
-	});
 
     $('.main-slider').slick({
         dots: true,
@@ -67,57 +62,18 @@ $(function(){
         ]
     });
 
-	/* components */
-	
-	/*
-	
-	if($('.styled').length) {
-		$('.styled').styler();
-	};
-	if($('.fancybox').length) {
-		$('.fancybox').fancybox({
-			margin  : 10,
-			padding  : 10
-		});
-	};
-	if($('.slick-slider').length) {
-		$('.slick-slider').slick({
-			dots: true,
-			infinite: false,
-			speed: 300,
-			slidesToShow: 4,
-			slidesToScroll: 4,
-			responsive: [
-				{
-				  breakpoint: 1024,
-				  settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true
-				  }
-				},
-				{
-				  breakpoint: 600,
-				  settings: "unslick"
-				}				
-			]
-		});
-	};
-	if($('.scroll').length) {
-		$(".scroll").mCustomScrollbar({
-			axis:"x",
-			theme:"dark-thin",
-			autoExpandScrollbar:true,
-			advanced:{autoExpandHorizontalScroll:true}
-		});
-	};
-	
-	*/
-	
-	/* components */
-	
-	
+
+
+
+    $('.grid').masonry({
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: 325,
+        gutter: 20,
+        fitWidth: true
+    });
+
+
 
 });
 
